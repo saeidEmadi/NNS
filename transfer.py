7be1acb48aa981a12d178ed7340430f5dbb30a34
saeid <emadisaeidabad@gmail.com>
2024-04-30 09:20:31 +1030

    transfer.py for run

---
+++ transfer.py
@@ -0,0 +1,11 @@
+class Transfer():
+    """ Transfer little of knowledge 
+        Neural Network Surgery (NNS)
+        Transfer sub network """
+    
+    def __init__(self) -> None:
+        pass
+    
+if __name__ == "__main__" :
+    """ CLI mode run """
+    print("\n[Transfer CLI mode]\n")
